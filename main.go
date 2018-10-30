bce80ff292444674bd261ba62746f7218f38bdbe
jezman <dev.jezman@gmail.com>
2018-10-30 13:49:48 +0300

    Companies list.

--- main.go
+++ main.go
@@ -1,26 +1,72 @@
 package main
 
 import (
-  "net/http"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
-  "github.com/gin-gonic/gin"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/gin-gonic/gin"
+	"github.com/jezman/gorion/models"
 )
 
-var router *gin.Engine
+var (
+	db      *models.DB
+	router  *gin.Engine
+	err     error
+	env     models.Datastore
+	timeNow = time.Now().Local()
+)
+
+func initDB() (db *models.DB) {
+	dsn := os.Getenv("BOLID_DSN")
+
+	if db, err = models.OpenDB(dsn); err != nil {
+		panic(err)
+	}
+
+	env = db
+	return
+}
+
+func indexHandler(c *gin.Context) {
+	render(c, gin.H{"title": "Главная страница"}, "index.html")
+}
+
+func companyHandler(c *gin.Context) {
+	companies, err := env.Company()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	render(c, gin.H{
+		"title":   "Список компаний",
+		"payload": companies}, "companies.html")
+
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
+	}
+
+}
 
 func main() {
-  router = gin.Default()
-  router.LoadHTMLGlob("templates/*")
+	initDB()
 
-  router.GET("/", func(c *gin.Context) {
-    c.HTML(
-      http.StatusOK,
-      "index.html",
-      gin.H{
-        "title": "Главная страница",
-      },
-    )
+	router = gin.Default()
+	router.LoadHTMLGlob("templates/*")
 
-  })
-  router.Run()
+	router.GET("/", indexHandler)
+	router.GET("/list/company", companyHandler)
+
+	router.Run()
 }
